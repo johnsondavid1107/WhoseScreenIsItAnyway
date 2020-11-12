65cac8779869fd61f18de5d78eb69e42e18dcea8
Ness <johnsondavid1107@gmail.com>
2020-11-12 14:26:03 -0500

    added more buttons and fluid transitioning when reloading onclick

--- class.js
+++ class.js
@@ -1,26 +1,43 @@
 $(document).ready(function () {
-
+   
+    var students = ["Aaron", "Akash", "Anthony", "David B.",
+    "David J.", "Hever", "Julia", "Julie", "Lohelani", "Maryam", "Richard", "Andrea"];
     
 
+    var newKid = [];
     function textToPage() {
-
-        var students = ["Aaron", "Akash", "Anthony", "David B.",
-            "David J.", "Hever", "Julia", "Julie", "Lohelani", "Maryam", "Richard", "Andrea"];
-
+        
         var random = Math.floor(Math.random() * students.length);
 
+        let choice = students[random];
 
-        $(".big").html(students[random]);
+        $(".big").html(choice);
         $("#spot").html("");
         $("#fade").addClass("fade-in");
         $("#fade").html("You get to share!");
         $(".big").addClass("fade-in");
         $("#topdog").addClass("jumbotron jumbotron-fluid blink-bg");
-        $(".display-4").html("CONGRATULATONS!!")
+        $(".display-4").html("CONGRATULATIONS!!")
 
 
         console.log("This has been clicked");
         console.log(students[random]);
+
+        $(".showButton").html("<button>Show The Blessed</button>");
+        document.getElementById("show").style.display = "inline-block";
+        const selection = students.filter(
+            (item) => {
+                return item !== students[random];
+            }
+        )
+        // students.splice(students[random],1);
+
+
+        console.log(students);
+        console.log(selection);
+       
+       
+
 
     }
 
@@ -35,10 +52,12 @@ $(document).ready(function () {
         $("#spot").html("Whose Screen Is It Anyway?...")
         $("#topdog").removeClass("jumbotron jumbotron-fluid blink-bg");
         $(".display-4").html("")
+        document.getElementById("start").style.display = "inline-block";
+        document.getElementById("show").style.display = "none";
     });
 
 
-   
+
 
 
 
