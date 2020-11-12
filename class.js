7a39974720ba778014625beabc02e53b4749ef13
Ness <johnsondavid1107@gmail.com>
2020-11-12 11:58:24 -0500

    added code to dynamically add pick to page, css styling, and onclick events

--- class.js
+++ class.js
@@ -1,21 +1,45 @@
+$(document).ready(function () {
+
+    
+
+    function textToPage() {
+
+        var students = ["Aaron", "Akash", "Anthony", "David B.",
+            "David J.", "Hever", "Julia", "Julie", "Lohelani", "Maryam", "Richard", "Andrea"];
+
+        var random = Math.floor(Math.random() * students.length);
 
 
-function textToPage() {
-
-    var students = ["Aaron", "Akash", "Anthony", "David B.",
-        "David J.", "Hever", "Julia", "Julie", "Lohelani", "Maryam", "Richard", "Andrea"];
-
-    var random = Math.floor(Math.random() * students.length);
-
-
-    $(".big").html(students[random]);
-    console.log("This has been clicked");
-
-}
+        $(".big").html(students[random]);
+        $("#spot").html("");
+        $("#fade").addClass("fade-in");
+        $("#fade").html("You get to share!");
+        $(".big").addClass("fade-in");
+        $("#topdog").addClass("jumbotron jumbotron-fluid blink-bg");
+        $(".display-4").html("CONGRATULATONS!!")
 
 
-$("button").on("click", textToPage);
+        console.log("This has been clicked");
+        console.log(students[random]);
+
+    }
+
+
+    $(".clack").on("click", textToPage);
+    $(".redo").on("click", function () {
+
+        $(".big").html("");
+        $("#fade").html("");
+        $(".big").removeClass("fade-in");
+        $("#fade").removeClass("fade-in");
+        $("#spot").html("Whose Screen Is It Anyway?...")
+        $("#topdog").removeClass("jumbotron jumbotron-fluid blink-bg");
+        $(".display-4").html("")
+    });
+
+
+   
 
 
 
-
+});
